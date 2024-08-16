87fa4706904a0d42604d1101a169c4057639fca1
Vincenzo <lisitano95@gmail.com>
2024-08-16 14:49:25 +0200

    Swipe Start

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ let observer = new IntersectionObserver( (entris)=>{
             creaIntervall(30, firnum, 100);
             creaIntervall(100, secnum, 50);
             creaIntervall(150, thdnum, 20);
-            comferm = false;
+            conferm = false;
         }
         
     })
@@ -78,3 +78,95 @@ let observer = new IntersectionObserver( (entris)=>{
 
 
 observer.observe(first)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// swiper
+    
+let reviw=[
+    {user: 'Fatima', descrizione: `Ottimi Prodotti`, rank:5},
+    {user: 'Vincenzo', descrizione: `prodotti rovinati e arrivati in ritardo`, rank:1},
+    {user: 'Antonella', descrizione: `Qualità prezzo`, rank:3},
+]
+
+
+let Wrap = document.querySelector('.swiper-wrapper')
+
+reviw.forEach( (recensione)=>{
+    let div = document.createElement('div');
+    div.classList.add('swiper-slide')
+    div.innerHTML= ` 
+    
+                    <div class="card-rec">
+                            <p class="lead text-center p2p">
+                               ${recensione.descrizione}
+                            </p>
+                            <p class="h4 text-center">${recensione.user}</p>
+                            <div class="d-flex justify-content-center star">
+                              
+                            </div>
+                        </div>
+                      </div>`;
+                Wrap.appendChild(div);
+    
+});
+
+
+let stelle = document.querySelectorAll('.star');
+
+stelle.forEach((star, index)=>{
+    for(let i = 1; i <= reviw[index].rank; i++ ){
+        let icon = document.createElement('i');
+        icon.classList.add('fa-solid', 'fa-star');
+        star.appendChild(icon)
+    }
+
+    let diff = 5 - reviw[index].rank;
+
+    for(let i = 1; i <= diff; i++ ){
+        let icon = document.createElement('i');
+        icon.classList.add('fa-regular', 'fa-star');
+        star.appendChild(icon)
+    }
+
+});
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: "coverflow",
+      grabCursor: true,
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
